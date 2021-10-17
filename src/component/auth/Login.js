ab6380c3f033c0b1ec333197eb6646140e2de25b
tessema2021 <ninafuchere@gmail.com>
2021-10-17 10:27:55 -0500

    login and Register form added

---
+++ src/component/auth/Login.js
@@ -0,0 +1,73 @@
+import React, { useRef, useState } from "react"
+import { Link, useHistory } from "react-router-dom";
+import "./Login.css"
+
+
+export const Login = () => {
+    const [loginUser, setLoginUser] = useState({ email: "" })
+    const [existDialog, setExistDialog] = useState(false)
+
+    const history = useHistory()
+
+    const handleInputChange = (event) => {
+        const newUser = { ...loginUser }
+        newUser[event.target.id] = event.target.value
+        setLoginUser(newUser)
+    }
+
+
+    const existingUserCheck = () => {
+        // If your json-server URL is different, please change it below!
+        return fetch(`http://localhost:8088/users?email=${loginUser.email}`)
+            .then(res => res.json())
+            .then(user => user.length ? user[0] : false)
+    }
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+
+        existingUserCheck()
+            .then(exists => {
+                if (exists) {
+                    // The user id is saved under the key nutshell_user in session Storage. Change below if needed!
+                    sessionStorage.setItem("medical_appointment_user", exists.id)
+                    history.push("/")
+                } else {
+                    setExistDialog(true)
+                }
+            })
+    }
+
+    return (
+        <main className="container--login">
+            <dialog className="dialog dialog--auth" open={existDialog}>
+                <div>User does not exist</div>
+                <button className="button--close" onClick={e => setExistDialog(false)}>Close</button>
+            </dialog>
+            <section>
+                <form className="form--login" onSubmit={handleLogin}>
+                    <h3 className="main_login">Complete The Appointment Form</h3>
+                    <h2 className="please_sign_in">Please sign in</h2>
+                    <fieldset className="login_form">
+                        <label htmlFor="inputEmail"> Email address </label>
+                        <input type="email"
+                            id="email"
+                            className="form-control"
+                            placeholder="Email address"
+                            required autoFocus
+                            value={loginUser.email}
+                            onChange={handleInputChange} />
+                    </fieldset>
+                    <fieldset>
+                        <button className="sign_in" type="submit">
+                            Sign in
+                        </button>
+                    </fieldset>
+                </form>
+            </section>
+            <section className="register_link">
+                <Link to="/register">Register for an account</Link>
+            </section>
+        </main>
+    )
+}
